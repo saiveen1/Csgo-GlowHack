51c4b30b598f8025e73d18e66a8b12dd370b56b9
saiveen1 <34472475+saiveen1@users.noreply.github.com>
2021-08-07 01:04:00 +0800

    Update glow.py
    Update the glow_manger struct and add new pattern

--- glow.py
+++ glow.py
@@ -26,13 +26,13 @@ def key_pressed(key):
     return GetAsyncKeyState(key) & 1 == 1
 
 
-def glow(h_game: pymem, glow_manager, entity_glow, color: ColorRGBA):
-    h_game.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(color.Red))
-    h_game.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(color.Green))
-    h_game.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(color.Blue))
-    h_game.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(color.Alpha))
-    h_game.write_uchar(glow_manager + entity_glow * 0x38 + 0x24, 1)
-    h_game.write_uchar(glow_manager + entity_glow * 0x38 + 0x25, 0)
+def glow(h_game: pymem, red_offset, glow_manager, entity_glow, color: ColorRGBA):
+    h_game.write_float(glow_manager + entity_glow * 0x38 + red_offset, float(color.Red))
+    h_game.write_float(glow_manager + entity_glow * 0x38 + red_offset + 0x4, float(color.Green))
+    h_game.write_float(glow_manager + entity_glow * 0x38 + red_offset + 0x8, float(color.Blue))
+    h_game.write_float(glow_manager + entity_glow * 0x38 + red_offset + 0xc, float(color.Alpha))
+    h_game.write_uchar(glow_manager + entity_glow * 0x38 + red_offset + 0x20, 1)
+    h_game.write_uchar(glow_manager + entity_glow * 0x38 + red_offset + 0x21, 0)
 
 
 def main():
@@ -47,26 +47,26 @@ def main():
     local_ptr_4 = client.lpBaseOfDll + re.search(rb'\x42\x56\x8d\x34\x85.{4}', client_module).start() + 5
     local_addr = handle.read_uint(local_ptr_4) + 4
     glow_manger_addr = client.lpBaseOfDll + re.search(rb'\x0f\x11\x05.{4}\x83\xc8\x01', client_module).start() + 3
-    entity_list_addr = client.lpBaseOfDll + re.search(rb'\x8b\x0d.{4}\xf3\x0f\x11\x45\xf4\x85\xc0', client_module).start() + 2
+    entity_list_addr = client.lpBaseOfDll + re.search(rb'\x8b\x0d.{4}\xf3\x0f\x11\x45\xf4\x85\xc0',
+                                                      client_module).start() + 2
     if entity_list_addr == 0:
-        entity_list_addr = client.lpBaseOfDll + re.search(rb'\x8b\x0d.{4}\x85\xc9\x74\x07\x8b\x01\xff\x50\x1c', client_module).start() + 2
+        entity_list_addr = client.lpBaseOfDll + re.search(rb'\x8b\x0d.{4}\x85\xc9\x74\x07\x8b\x01\xff\x50\x1c',
+                                                          client_module).start() + 2
     glow_index_addr = client.lpBaseOfDll + re.search(rb'\x8B\x7d\xec\x8b\xb3.{4}', client_module).start() + 5
     health_addr = client.lpBaseOfDll + re.search(rb'\x83\xb9.{4}\x00\x7f\x2d\x8b\x01', client_module).start() + 2
     team_addr = client.lpBaseOfDll + re.search(rb'\xcc\x8b\x89.{4}\xe9.{4}\xcc', client_module).start() + 3
+    glow_red_addr = client.lpBaseOfDll + re.search(rb'\x8b\x00\xf3\x0f\x11\x44\xc8.\xf3\x0f\x10\x44\x24', client_module).start() + 7
 
     glow_manager_ptr = handle.read_uint(glow_manger_addr)
     entity_list_ptr = handle.read_uint(entity_list_addr)
     m_glow_index = handle.read_uint(glow_index_addr)
     m_health = handle.read_uint(health_addr)
     m_team = handle.read_uint(team_addr)
-
-    print(hex(entity_list_addr))
-
+    red_offset = handle.read_uchar(glow_red_addr)
     print("Glow has launched.")
     print("Press F1 to show teammates.")
     print("Press F2 to turn the glow on/off.")
     print("Press END to quit.")
-
     try:
         while True:
             glow_manager = handle.read_uint(glow_manager_ptr)
@@ -94,14 +94,14 @@ def main():
                             entity_glow = handle.read_uint(entity + m_glow_index)
 
                             if entity_team == local_team:
-                                glow(handle, glow_manager, entity_glow, teammate_color)
+                                glow(handle, red_offset, glow_manager, entity_glow, teammate_color)
                             else:
                                 ent_health = handle.read_uint(entity + m_health)
                                 enemy_color.Green = 0.006 * ent_health
                                 if ent_health == 0x64:
                                     enemy_color.Green = 1
                                 enemy_color.Red = 1.0 - enemy_color.Green
-                                glow(handle, glow_manager, entity_glow, enemy_color)
+                                glow(handle, red_offset, glow_manager, entity_glow, enemy_color)
 
     finally:
         handle.close_process()
@@ -109,4 +109,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
